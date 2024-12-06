70fabd1b65b5a800f6ec7c6e56e709b2845274ed
anant2004 <anant2004infinity@gmail.com>
2024-12-06 07:41:59 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose, {
+    Schema
+} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true //if you want to make a field more search friendly make index true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true //if you want to make a field more search friendly make index true
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    coverimage: {
+        type: String,
+    },
+    watchhistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password: {
+        type: String,
+        reqired: [true, 'Password is required'],
+    },
+    refreshtoken: {
+        type: String
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre("save", async function (next) {
+    if (!this.isMofidied("password")) return next(); // this if checks that the encryption will run only when the password is modified
+
+    this.password = bcrypt.hash(this.password, 10) // here we are using bcrypt to encrypt our password just before saving it to the database
+    next()
+}) // it is a hook which will perform the code specified just before saving the data in the data base
+
+userSchema.methods.isPasswordCorrect = async function (password) { // this is a custom method to compare the user given password and the encrypted password
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET, {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
